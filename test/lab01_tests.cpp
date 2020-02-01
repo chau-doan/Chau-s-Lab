a5679e2d04ebd31919f1d807e55b31727501f3d9
chau.doan@sjsu.edu <Chaudoan2110>
2020-01-31 20:40:09 -0800

    Added 5 more passing test

--- test/lab01_tests.cpp
+++ test/lab01_tests.cpp
@@ -32,6 +32,11 @@ TEST(crashTest , constructorTest) {
     lab1::expressionstream stream2("1   + 2 *4+(2*3)");
 }
 
+TEST(crashTest , constructorTest2) {
+    lab1::expressionstream stream3("-1 + -2");
+    lab1::expressionstream stream4("-1-(-1-(1--1)-1)-1");
+}
+
 TEST_F(Lab01Fixture, basicNextToken) {
     //1+2+3
     EXPECT_EQ("1", stream1->get_next_token());
@@ -347,4 +352,245 @@ TEST_F(Lab01Fixture, CheckNextType) {
     EXPECT_TRUE(stream4->next_token_is_int());
     EXPECT_FALSE(stream4->next_token_is_op());
     stream4->get_next_token();
+}
+
+TEST_F(Lab01Fixture, multiDigitNextToken1) {
+    delete stream1;
+    delete stream2;
+    delete stream3;
+    delete stream4;
+
+    stream1 = new lab1::expressionstream("123+1654*121+1321--455");
+    stream2 = new lab1::expressionstream("2123+151+(15248-4154--485)");
+    stream3 = new lab1::expressionstream("1231+1564--41545");
+    stream4 = new lab1::expressionstream("-15+515*(21+(21*51+31)*-45)-1");
+
+    //123+1654*121+1321--455
+    EXPECT_EQ("123", stream1->get_next_token());
+    EXPECT_EQ("+", stream1->get_next_token());
+    EXPECT_EQ("1654", stream1->get_next_token());
+    EXPECT_EQ("*", stream1->get_next_token());
+    EXPECT_EQ("121", stream1->get_next_token());
+    EXPECT_EQ("+", stream1->get_next_token());
+    EXPECT_EQ("1321", stream1->get_next_token());
+    EXPECT_EQ("-", stream1->get_next_token());
+    EXPECT_EQ("-455", stream1->get_next_token());
+    EXPECT_EQ("\0", stream1->get_next_token());
+
+    //2123+151+(15248-4154--485)
+    EXPECT_EQ("2123", stream2->get_next_token());
+    EXPECT_EQ("+", stream2->get_next_token());
+    EXPECT_EQ("151", stream2->get_next_token());
+    EXPECT_EQ("+", stream2->get_next_token());
+    EXPECT_EQ("(", stream2->get_next_token());
+    EXPECT_EQ("15248", stream2->get_next_token());
+    EXPECT_EQ("-", stream2->get_next_token());
+    EXPECT_EQ("4154", stream2->get_next_token());
+    EXPECT_EQ("-", stream2->get_next_token());
+    EXPECT_EQ("-485", stream2->get_next_token());
+    EXPECT_EQ(")", stream2->get_next_token());
+    EXPECT_EQ("\0", stream2->get_next_token());
+
+    //1231+1564--41545
+    EXPECT_EQ("1231", stream3->get_next_token());
+    EXPECT_EQ("+", stream3->get_next_token());
+    EXPECT_EQ("1564", stream3->get_next_token());
+    EXPECT_EQ("-", stream3->get_next_token());
+    EXPECT_EQ("-41545", stream3->get_next_token());
+    EXPECT_EQ("\0", stream3->get_next_token());
+
+    //-15+515*(21+(21*51+31)*-45)-1
+    EXPECT_EQ("-15", stream4->get_next_token());
+    EXPECT_EQ("+", stream4->get_next_token());
+    EXPECT_EQ("515", stream4->get_next_token());
+    EXPECT_EQ("*", stream4->get_next_token());
+    EXPECT_EQ("(", stream4->get_next_token());
+    EXPECT_EQ("21", stream4->get_next_token());
+    EXPECT_EQ("+", stream4->get_next_token());
+    EXPECT_EQ("(", stream4->get_next_token());
+    EXPECT_EQ("21", stream4->get_next_token());
+    EXPECT_EQ("*", stream4->get_next_token());
+    EXPECT_EQ("51", stream4->get_next_token());
+    EXPECT_EQ("+", stream4->get_next_token());
+    EXPECT_EQ("31", stream4->get_next_token());
+    EXPECT_EQ(")", stream4->get_next_token());
+    EXPECT_EQ("*", stream4->get_next_token());
+    EXPECT_EQ("-45", stream4->get_next_token());
+    EXPECT_EQ(")", stream4->get_next_token());
+    EXPECT_EQ("-", stream4->get_next_token());
+    EXPECT_EQ("1", stream4->get_next_token());
+    EXPECT_EQ("\0", stream4->get_next_token());
+
+}
+
+TEST_F(Lab01Fixture, CheckNextTypestream1) {
+    delete stream1;
+
+    stream1 = new lab1::expressionstream("234+45*65+1--5");
+
+    //123+1654*121+1321--455
+    EXPECT_TRUE(stream1->next_token_is_int());
+    EXPECT_FALSE(stream1->next_token_is_op());
+    EXPECT_FALSE(stream1->next_token_is_paren_close());
+    EXPECT_FALSE(stream1->next_token_is_paren_open());
+    stream1->get_next_token();
+
+    EXPECT_TRUE(stream1->next_token_is_op());
+    EXPECT_FALSE(stream1->next_token_is_int());
+    EXPECT_FALSE(stream1->next_token_is_paren_close());
+    EXPECT_FALSE(stream1->next_token_is_paren_open());
+    stream1->get_next_token();
+
+    EXPECT_FALSE(stream1->next_token_is_op());
+    EXPECT_TRUE(stream1->next_token_is_int());
+    EXPECT_FALSE(stream1->next_token_is_paren_close());
+    EXPECT_FALSE(stream1->next_token_is_paren_open());
+    stream1->get_next_token();
+
+    EXPECT_TRUE(stream1->next_token_is_op());
+    EXPECT_FALSE(stream1->next_token_is_int());
+    EXPECT_FALSE(stream1->next_token_is_paren_close());
+    EXPECT_FALSE(stream1->next_token_is_paren_open());
+    stream1->get_next_token();
+
+    EXPECT_FALSE(stream1->next_token_is_op());
+    EXPECT_TRUE(stream1->next_token_is_int());
+    EXPECT_FALSE(stream1->next_token_is_paren_close());
+    EXPECT_FALSE(stream1->next_token_is_paren_open());
+    stream1->get_next_token();
+
+    EXPECT_TRUE(stream1->next_token_is_op());
+    EXPECT_FALSE(stream1->next_token_is_int());
+    EXPECT_FALSE(stream1->next_token_is_paren_close());
+    EXPECT_FALSE(stream1->next_token_is_paren_open());
+    stream1->get_next_token();
+
+    EXPECT_FALSE(stream1->next_token_is_op());
+    EXPECT_TRUE(stream1->next_token_is_int());
+    EXPECT_FALSE(stream1->next_token_is_paren_close());
+    EXPECT_FALSE(stream1->next_token_is_paren_open());
+    stream1->get_next_token();
+
+    EXPECT_TRUE(stream1->next_token_is_op());
+    EXPECT_FALSE(stream1->next_token_is_int());
+    EXPECT_FALSE(stream1->next_token_is_paren_close());
+    EXPECT_FALSE(stream1->next_token_is_paren_open());
+    stream1->get_next_token();
+
+    EXPECT_FALSE(stream1->next_token_is_op());
+    EXPECT_TRUE(stream1->next_token_is_int());
+    EXPECT_FALSE(stream1->next_token_is_paren_close());
+    EXPECT_FALSE(stream1->next_token_is_paren_open());
+    stream1->get_next_token();
+}
+
+TEST_F(Lab01Fixture, stream2checkNextToken) {
+    delete stream2;
+    stream2 = new lab1::expressionstream("13+11+(18-44--45)");
+
+    //13
+    EXPECT_TRUE(stream2->next_token_is_int());
+    EXPECT_EQ("13", stream2->get_next_token());
+
+    //+
+    EXPECT_TRUE(stream2->next_token_is_op());
+    EXPECT_EQ("+", stream2->get_next_token());
+
+    //11
+    EXPECT_TRUE(stream2->next_token_is_int());
+    EXPECT_EQ("11", stream2->get_next_token());
+
+    //+
+    EXPECT_TRUE(stream2->next_token_is_op());
+    EXPECT_EQ("+", stream2->get_next_token());
+
+    //(
+    EXPECT_TRUE(stream2->next_token_is_paren_open());
+    EXPECT_EQ("(", stream2->get_next_token());
+
+    //18
+    EXPECT_TRUE(stream2->next_token_is_int());
+    EXPECT_EQ("18", stream2->get_next_token());
+
+    //-
+    EXPECT_TRUE(stream2->next_token_is_op());
+    EXPECT_EQ("-", stream2->get_next_token());
+
+
+    //44
+    EXPECT_TRUE(stream2->next_token_is_int());
+    EXPECT_EQ("44", stream2->get_next_token());
+
+    //-
+    EXPECT_TRUE(stream2->next_token_is_op());
+    EXPECT_EQ("-", stream2->get_next_token());
+
+    //-45
+    EXPECT_TRUE(stream2->next_token_is_int());
+    EXPECT_EQ("-45", stream2->get_next_token());
+
+    //)
+    EXPECT_TRUE(stream2->next_token_is_paren_close());
+    EXPECT_EQ(")", stream2->get_next_token());
+}
+
+TEST_F(Lab01Fixture, stream3check){
+    delete stream3;
+    stream3 = new lab1::expressionstream("-12+23*45/(12+3)");
+
+    //-12
+    EXPECT_TRUE(stream3->next_token_is_int());
+    stream3->get_next_token();
+    EXPECT_EQ("-12",stream3->get_current_token());
+
+    //+
+    EXPECT_TRUE(stream3->next_token_is_op());
+    stream3->get_next_token();
+    EXPECT_EQ("+",stream3->get_current_token());
+
+    //23
+    EXPECT_TRUE(stream3->next_token_is_int());
+    stream3->get_next_token();
+    EXPECT_EQ("23",stream3->get_current_token());
+
+    //*
+    EXPECT_TRUE(stream3->next_token_is_op());
+    stream3->get_next_token();
+    EXPECT_EQ("*",stream3->get_current_token());
+
+    //45
+    EXPECT_TRUE(stream3->next_token_is_int());
+    stream3->get_next_token();
+    EXPECT_EQ("45",stream3->get_current_token());
+
+    // /
+    EXPECT_TRUE(stream3->next_token_is_op());
+    stream3->get_next_token();
+    EXPECT_EQ("/",stream3->get_current_token());
+
+    //(
+    EXPECT_TRUE(stream3->next_token_is_paren_open());
+    stream3->get_next_token();
+    EXPECT_EQ("(",stream3->get_current_token());
+
+    //12
+    EXPECT_TRUE(stream3->next_token_is_int());
+    stream3->get_next_token();
+    EXPECT_EQ("12",stream3->get_current_token());
+
+    //+
+    EXPECT_TRUE(stream3->next_token_is_op());
+    stream3->get_next_token();
+    EXPECT_EQ("+",stream3->get_current_token());
+
+    //3
+    EXPECT_TRUE(stream3->next_token_is_int());
+    stream3->get_next_token();
+    EXPECT_EQ("3",stream3->get_current_token());
+
+    //)
+    EXPECT_TRUE(stream3->next_token_is_paren_close());
+    stream3->get_next_token();
+    EXPECT_EQ(")",stream3->get_current_token());
+
 }
